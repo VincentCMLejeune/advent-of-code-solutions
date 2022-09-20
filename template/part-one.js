92fc6221dae5153eeda6b5d2c3dd17e886b3cef7
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-09-20 17:34:23 +0200

    Modified template

--- template/part-one.js
+++ template/part-one.js
@@ -5,9 +5,7 @@ const Game = require("./game");
 for (let sampleInput of sampleInputs) {
   let sample_game = new Game(sampleInput.input);
   let sample_part_one = sample_game.customMethod();
-  if (sampleInput.partOne !== undefined) {
-    assert.strictEqual(sample_part_one, sampleInput.partOne);
-  }
+  assert.strictEqual(sample_part_one, sampleInput.partOne);
 }
 
 let game = new Game(input);
