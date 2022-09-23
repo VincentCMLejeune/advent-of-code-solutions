79388b21775146f6efb4aab101a58137bb89437c
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-09-23 12:11:05 +0200

    2021 day 04 : fetch parse and process data(with empty boards)

--- 2021/04/bingo.js
+++ 2021/04/bingo.js
@@ -2,34 +2,42 @@ class Bingo {
   constructor(raw_data) {
     let processed_data = this.parseInfos(raw_data);
     this.numbers = processed_data.numbers;
-    this.boards = processed_data.boards;
+    this.fullBoards = processed_data.fullBoards;
+    this.emptyBoards = processed_data.emptyBoards;
   }
 
   partOneScore() {
     console.log(this.numbers);
-    console.log(this.boards);
+    console.log(this.fullBoards);
+    console.log(this.emptyBoards);
     return true;
   }
 
   parseInfos(raw_data) {
     let processed_data = {};
     processed_data.numbers = raw_data[0].split(",").map(Number);
-    processed_data.boards = [];
-    let currentBoard = [];
+    processed_data.fullBoards = [];
+    processed_data.emptyBoards = [];
+    let currentFullBoard = [];
+    let currentEmptyBoard = [];
     for (let i = 2; i < raw_data.length; i++) {
       if (raw_data[i] === "") {
-        processed_data.boards.push(currentBoard);
-        currentBoard = [];
+        processed_data.fullBoards.push(currentFullBoard);
+        processed_data.emptyBoards.push(currentEmptyBoard);
+        currentFullBoard = [];
+        currentEmptyBoard = [];
       } else {
-        currentBoard.push(
+        currentFullBoard.push(
           raw_data[i]
             .split(" ")
             .filter((elem) => elem !== "")
             .map(Number)
         );
+        currentEmptyBoard.push(["", "", "", "", ""]);
       }
     }
-    processed_data.boards.push(currentBoard);
+    processed_data.fullBoards.push(currentFullBoard);
+    processed_data.emptyBoards.push(currentEmptyBoard);
     return processed_data;
   }
 }
