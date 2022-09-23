40d2669710f40b38b75e2a637bcf7ec12ab4c188
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-09-23 13:28:26 +0200

    2021 day 04 part 1

--- 2021/04/bingo.js
+++ 2021/04/bingo.js
@@ -6,11 +6,75 @@ class Bingo {
     this.emptyBoards = processed_data.emptyBoards;
   }
 
+  playBingo() {
+    for (let pickedNumber of this.numbers) {
+      for (let i = 0; i < this.fullBoards.length; i++) {
+        for (let j = 0; j < 5; j++) {
+          for (let k = 0; k < 5; k++) {
+            if (this.fullBoards[i][j][k] === pickedNumber) {
+              this.emptyBoards[i][j][k] = pickedNumber;
+              let winningBoard = this.checkWinner();
+              if (winningBoard !== false) {
+                return {
+                  winningBoard: winningBoard,
+                  winningFullBoard: this.fullBoards[i],
+                  lastPickedNumber: pickedNumber,
+                };
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  checkWinner() {
+    for (let board of this.emptyBoards) {
+      for (let i = 0; i < 5; i++) {
+        if (
+          board[i][0] !== "" &&
+          board[i][1] !== "" &&
+          board[i][2] !== "" &&
+          board[i][3] !== "" &&
+          board[i][4] !== ""
+        ) {
+          return board;
+        }
+      }
+      for (let i = 0; i < 5; i++) {
+        if (
+          board[0][i] !== "" &&
+          board[1][i] !== "" &&
+          board[2][i] !== "" &&
+          board[3][i] !== "" &&
+          board[4][i] !== ""
+        ) {
+          return board;
+        }
+      }
+    }
+    return false;
+  }
+
+  sumUnMarkedNumbers(fullBoard, markedBoard) {
+    let sum = 0;
+    let flattenFullBoard = fullBoard.flat();
+    let flattenMarkedBoard = markedBoard.flat();
+    for (let i = 0; i < flattenMarkedBoard.length; i++) {
+      if (flattenMarkedBoard[i] === "") {
+        sum += flattenFullBoard[i];
+      }
+    }
+    return sum;
+  }
+
   partOneScore() {
-    console.log(this.numbers);
-    console.log(this.fullBoards);
-    console.log(this.emptyBoards);
-    return true;
+    let result = this.playBingo();
+    let sumUnmarkedNumbers = this.sumUnMarkedNumbers(
+      result.winningFullBoard,
+      result.winningBoard
+    );
+    return result.lastPickedNumber * sumUnmarkedNumbers;
   }
 
   parseInfos(raw_data) {
