72df5a82da5992a9408590fa64409db6e0543bff
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-09-28 08:24:04 +0200

    Updated template

--- template/first-part.js
+++ template/first-part.js
@@ -2,12 +2,13 @@ const assert = require("assert");
 const { input, sampleInputs } = require("./data");
 const AdventOfCode = require("./index");
 
-for (let sampleInput of sampleInputs) {
-  let sample_game = new AdventOfCode(sampleInput.input);
+for (let i = 0 ; i < sampleInputs.length ; i++) {
+  let sample_game = new AdventOfCode(sampleInputs[i].input);
   let sample_part_one = sample_game.part_one();
-  assert.strictEqual(sample_part_one, sampleInput.part_one);
+  assert.strictEqual(sample_part_one, sampleInputs[i].part_one);
+  console.log(`Test ${i + 1} successful`);
 }
 
 let game = new AdventOfCode(input);
 let res = game.part_one();
-console.log(res);
+console.log(`Part one answer: ${res}`);
--- template/second-part.js
+++ template/second-part.js
@@ -2,12 +2,13 @@ const assert = require("assert");
 const { input, sampleInputs } = require("./data");
 const AdventOfCode = require("./index");
 
-for (let sampleInput of sampleInputs) {
-  let sample_game = new AdventOfCode(sampleInput.input);
+for (let i = 0 ; i < sampleInputs.length ; i++) {
+  let sample_game = new AdventOfCode(sampleInputs[i].input);
   let sample_part_two = sample_game.part_two();
-  assert.strictEqual(sample_part_two, sampleInput.part_two);
+  assert.strictEqual(sample_part_two, sampleInputs[i].part_two);
+  console.log(`Test ${i + 1} successful`);
 }
 
 let game = new AdventOfCode(input);
 let res = game.part_two();
-console.log(res);
+console.log(`Part two answer: ${res}`);
