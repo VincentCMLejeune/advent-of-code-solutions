94ea33e385a40d38cab7b4977d19ac684f528ad9
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-14 19:50:23 +0200

    2015 day 03 part 2

--- 2015/03/data.js
+++ 2015/03/data.js
@@ -3,23 +3,23 @@ const fs = require("fs");
 
 const input = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf8")
-  .toString()
+  .toString();
 
 const sampleInputs = [
   {
     input: `^v`,
     part_one: 2,
-    part_two: undefined,
+    part_two: 3,
   },
   {
     input: `^>v<`,
     part_one: 4,
-    part_two: undefined,
+    part_two: 3,
   },
   {
     input: `^v^v^v^v^v`,
     part_one: 2,
-    part_two: undefined,
+    part_two: 11,
   },
 ];
 
--- 2015/03/index.js
+++ 2015/03/index.js
@@ -46,7 +46,78 @@ class AdventOfCode {
   }
 
   part_two() {
-    return true;
+    let children = 1;
+    const santasWereHere = {
+      0: {
+        0: true,
+      },
+    };
+    const santaLocation = {
+      x: 0,
+      y: 0,
+    };
+    const roboLocation = {
+      x: 0,
+      y: 0,
+    };
+    for (let i = 0; i < this.instructions.length; i += 2) {
+      const santaInstruction = this.instructions[i];
+      switch (santaInstruction) {
+        case "^":
+          santaLocation.y--;
+          break;
+        case "v":
+          santaLocation.y++;
+          break;
+        case "<":
+          santaLocation.x--;
+          break;
+        case ">":
+          santaLocation.x++;
+          break;
+        default:
+          throw new Error(
+            "Instructions unclear, stuck my dick in the ceiling fan"
+          );
+      }
+      if (!santasWereHere[santaLocation.x]) {
+        children++;
+        santasWereHere[santaLocation.x] = {};
+        santasWereHere[santaLocation.x][santaLocation.y] = true;
+      } else if (!santasWereHere[santaLocation.x][santaLocation.y]) {
+        children++;
+        santasWereHere[santaLocation.x][santaLocation.y] = true;
+      }
+
+      const robotInstruction = this.instructions[i + 1];
+      switch (robotInstruction) {
+        case "^":
+          roboLocation.y--;
+          break;
+        case "v":
+          roboLocation.y++;
+          break;
+        case "<":
+          roboLocation.x--;
+          break;
+        case ">":
+          roboLocation.x++;
+          break;
+        default:
+          throw new Error(
+            "Instructions unclear, stuck my dick in the ceiling fan"
+          );
+      }
+      if (!santasWereHere[roboLocation.x]) {
+        children++;
+        santasWereHere[roboLocation.x] = {};
+        santasWereHere[roboLocation.x][roboLocation.y] = true;
+      } else if (!santasWereHere[roboLocation.x][roboLocation.y]) {
+        children++;
+        santasWereHere[roboLocation.x][roboLocation.y] = true;
+      }
+    }
+    return children;
   }
 
   parseInfos(raw_data) {
