daa853e832ff94d58c6959e9f6bab9daef1eb621
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-18 11:26:56 +0200

    2020 day 04 part 2

--- 2020/04/index.js
+++ 2020/04/index.js
@@ -13,7 +13,80 @@ class AdventOfCode {
   }
 
   part_two() {
-    return true;
+    let validPassesCount = 0;
+    this.passes.forEach((pass) => {
+      if (this.hasAllRequiredFields(pass)) {
+        if (this.validByr(pass)) {
+          if (this.validIyr(pass)) {
+            if (this.validEyr(pass)) {
+              if (this.validHgt(pass)) {
+                if (this.validHcl(pass)) {
+                  if (this.validEcl(pass)) {
+                    if (this.validPid(pass)) {
+                      validPassesCount++;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+    return validPassesCount;
+  }
+
+  validPid(pass) {
+    const nums = "0123456789".split("");
+    return (
+      pass.pid.length === 9 &&
+      pass.pid.split("").every((char) => nums.indexOf(char) !== -1)
+    );
+  }
+
+  validEcl(pass) {
+    const validEcl = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+    return validEcl.indexOf(pass.ecl) !== -1;
+  }
+
+  validHcl(pass) {
+    const validHclChar = "0123456789abcdef".split("");
+    return (
+      pass.hcl.length === 7 &&
+      pass.hcl[0] === "#" &&
+      pass.hcl
+        .substring(1)
+        .split("")
+        .every((char) => validHclChar.indexOf(char) !== -1)
+    );
+  }
+
+  validHgt(pass) {
+    if (pass.hgt.endsWith("cm")) {
+      let size = Number(pass.hgt.split("cm")[0]);
+      return size >= 150 && size <= 193;
+    } else if (pass.hgt.endsWith("in")) {
+      let size = Number(pass.hgt.split("in")[0]);
+      return size >= 59 && size <= 76;
+    }
+    return false;
+  }
+
+  validEyr(pass) {
+    return pass.eyr >= 2020 && pass.eyr <= 2030;
+  }
+
+  validIyr(pass) {
+    return pass.iyr >= 2010 && pass.iyr <= 2020;
+  }
+
+  validByr(pass) {
+    return pass.byr >= 1920 && pass.byr <= 2002;
+  }
+
+  hasAllRequiredFields(pass) {
+    const requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    return requiredFields.every((field) => pass[field]);
   }
 
   parseInfos(raw_data) {
