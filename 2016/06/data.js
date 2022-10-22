55fa22599cd26b2908f0334698dbfbd06ff843c5
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-22 10:48:55 +0200

    2016 day 06 part 1

---
+++ 2016/06/data.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const sampleInputs = [
+  {
+    input: `eedadn
+    drvtee
+    eandsr
+    raavrd
+    atevrs
+    tsrnev
+    sdttsa
+    rasrtv
+    nssdts
+    ntnada
+    svetve
+    tesnvt
+    vntsnd
+    vrdear
+    dvrsen
+    enarar`
+      .split("\n")
+      .map((elem) => elem.trim()),
+    part_one: "easter",
+    part_two: undefined,
+  },
+  {
+    input: ``.split("\n").map((elem) => elem.trim()),
+    part_one: undefined,
+    part_two: undefined,
+  },
+];
+
+module.exports = {
+  input,
+  sampleInputs,
+};
---
+++ 2016/06/index.js
@@ -0,0 +1,36 @@
+class AdventOfCode {
+  constructor(raw_data) {
+    this.words = this.parseInfos(raw_data);
+  }
+
+  part_one() {
+    let wordRange = this.words[0].length;
+    let charMap = {};
+    for (let i = 0; i < wordRange; i++) {
+      charMap[i] = {};
+    }
+    for (let word of this.words) {
+      for (let i = 0; i < word.length; i++) {
+        charMap[i][word[i]] = charMap[i][word[i]] + 1 || 1;
+      }
+    }
+    let word = "";
+    for (let i = 0; i < wordRange; i++) {
+      const sortedKeys = Object.keys(charMap[i]).sort(
+        (a, b) => charMap[i][b] - charMap[i][a]
+      );
+      word += sortedKeys[0];
+    }
+    return word;
+  }
+
+  part_two() {
+    return true;
+  }
+
+  parseInfos(raw_data) {
+    return raw_data.map((line) => line.split(""));
+  }
+}
+
+module.exports = AdventOfCode;
