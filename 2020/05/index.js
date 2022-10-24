a2f9936fdb82694809d7ea971e7a72054c9f0364
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-10-24 18:17:33 +0200

    2020 day 05 part 1

---
+++ 2020/05/index.js
@@ -0,0 +1,58 @@
+class AdventOfCode {
+  constructor(raw_data) {
+    this.infos = this.parseInfos(raw_data);
+  }
+
+  part_one() {
+    const lines = [];
+    for (let info of this.infos) {
+      // console.log(info);
+      const row = this.getRow(info.slice(0, 7));
+      let line = this.getLine(info.slice(7));
+      lines.push(row * 8 + line);
+      // lines.push(row);
+    }
+    // console.log(lines);
+    return Math.max(...lines);
+  }
+
+  getRow(arr) {
+    let min = 0;
+    let max = 127;
+    for (let i = 0; i < 7; i++) {
+      let val = Math.pow(2, 6 - i);
+      if (arr[i] === "B") {
+        min += val;
+      } else if (arr[i] === "F") {
+        max -= val;
+      }
+      // console.log(arr[i], min, max);
+    }
+    return min;
+  }
+
+  getLine(arr) {
+    let min = 0;
+    let max = 127;
+    for (let i = 0; i < 3; i++) {
+      let val = Math.pow(2, 2 - i);
+      if (arr[i] === "R") {
+        min += val;
+      } else if (arr[i] === "L") {
+        max -= val;
+      }
+      // console.log(arr[i], min, max);
+    }
+    return min;
+  }
+
+  part_two() {
+    return true;
+  }
+
+  parseInfos(raw_data) {
+    return raw_data.map((line) => line.split(""));
+  }
+}
+
+module.exports = AdventOfCode;
