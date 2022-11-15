42ba853e279eaca9584c5cd1fc2d4cee257f8d41
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-11-15 23:22:08 +0100

    2015 day 13 part 1

---
+++ 2015/13/data.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const sampleInputs = [
+  {
+    input: `Alice would gain 54 happiness units by sitting next to Bob.
+    Alice would lose 79 happiness units by sitting next to Carol.
+    Alice would lose 2 happiness units by sitting next to David.
+    Bob would gain 83 happiness units by sitting next to Alice.
+    Bob would lose 7 happiness units by sitting next to Carol.
+    Bob would lose 63 happiness units by sitting next to David.
+    Carol would lose 62 happiness units by sitting next to Alice.
+    Carol would gain 60 happiness units by sitting next to Bob.
+    Carol would gain 55 happiness units by sitting next to David.
+    David would gain 46 happiness units by sitting next to Alice.
+    David would lose 7 happiness units by sitting next to Bob.
+    David would gain 41 happiness units by sitting next to Carol.`
+      .split("\n")
+      .map((elem) => elem.trim()),
+    part_one: 330,
+    part_two: undefined,
+  },
+  {
+    input: ``.split("\n").map((elem) => elem.trim()),
+    part_one: undefined,
+    part_two: undefined,
+  },
+];
+
+module.exports = {
+  input,
+  sampleInputs,
+};
---
+++ 2015/13/index.js
@@ -0,0 +1,56 @@
+class AdventOfCode {
+  constructor(raw_data) {
+    this.happiness_map = this.parseInfos(raw_data);
+    this.orders = [];
+  }
+
+  part_one() {
+    this.permutations(Object.keys(this.happiness_map));
+    let maxHappiness = -Infinity;
+    for (let order of this.orders) {
+      let curHappiness = this.getTotalHappiness(order);
+      maxHappiness = Math.max(maxHappiness, curHappiness);
+    }
+    return maxHappiness;
+  }
+
+  getTotalHappiness(arr) {
+    let happiness = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+      happiness += this.happiness_map[arr[i]][arr[i + 1]];
+      happiness += this.happiness_map[arr[i + 1]][arr[i]];
+    }
+    return happiness;
+  }
+
+  permutations(arr, r = []) {
+    if (arr.length === 0) {
+      r.push(r[0]);
+      this.orders.push(r);
+    } else {
+      const first = arr[0];
+      for (let i = 0; i <= r.length; i++) {
+        this.permutations(
+          arr.slice(1),
+          r.slice(0, i).concat([first]).concat(r.slice(i))
+        );
+      }
+    }
+  }
+
+  parseInfos(raw_data) {
+    const happiness_map = {};
+    for (let line of raw_data) {
+      line = line.split(" ");
+      const key = line[0];
+      let happiness = Number(line[3]);
+      if (line[2] === "lose") happiness *= -1;
+      const target = line[10].slice(0, -1);
+      if (!happiness_map[key]) happiness_map[key] = {};
+      happiness_map[key][target] = happiness;
+    }
+    return happiness_map;
+  }
+}
+
+module.exports = AdventOfCode;
