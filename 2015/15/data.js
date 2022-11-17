f3c00fd901b1fbf47629fc9b137579a4e60a6d06
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-11-17 11:47:44 +0100

    2015 day 15 part 1

---
+++ 2015/15/data.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const sampleInputs = [
+  {
+    input: `Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+    Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`.split("\n").map((elem) => elem.trim()),
+    part_one: 62842880,
+    part_two: undefined,
+  },
+  {
+    input: ``.split("\n").map((elem) => elem.trim()),
+    part_one: undefined,
+    part_two: undefined,
+  },
+];
+
+module.exports = {
+  input,
+  sampleInputs,
+};
---
+++ 2015/15/index.js
@@ -0,0 +1,72 @@
+class AdventOfCode {
+  constructor(raw_data) {
+    this.ingredients = this.parseInfos(raw_data);
+    this.recipes = this.iterate(this.ingredients.length);
+  }
+
+  part_one() {
+    let bestScore = -Infinity;
+    for (let recipe of this.recipes) {
+      let score = this.bake(recipe);
+      bestScore = Math.max(bestScore, score);
+    }
+    return bestScore;
+  }
+
+  bake(recipe) {
+    let capacity = this.add("capacity", recipe);
+    let durability = this.add("durability", recipe);
+    let flavor = this.add("flavor", recipe);
+    let texture = this.add("texture", recipe);
+    return capacity * durability * flavor * texture;
+  }
+
+  add(char, recipe) {
+    let score = 0;
+    for (let i = 0; i < recipe.length; i++) {
+      score += this.ingredients[i][char] * recipe[i];
+    }
+    return Math.max(score, 0);
+  }
+
+  iterate(len) {
+    let recipes = [];
+    if (len === 4) {
+      for (let i = 0; i <= 100; i++) {
+        for (let j = 0; j <= 100 - i; j++) {
+          for (let k = 0; k <= 100 - i - j; k++) {
+            let l = 100 - i - j - k;
+            recipes.push([i, j, k, l]);
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i <= 100; i++) {
+        let l = 100 - i;
+        recipes.push([i, l]);
+      }
+    }
+    return recipes;
+  }
+
+  part_two() {
+    return true;
+  }
+
+  parseInfos(raw_data) {
+    const parsed_data = [];
+    for (let line of raw_data) {
+      let newLine = {};
+      line = line.split(": ");
+      newLine.name = line[0];
+      for (let ingredient of line[1].split(", ")) {
+        ingredient = ingredient.split(" ");
+        newLine[ingredient[0]] = Number(ingredient[1]);
+      }
+      parsed_data.push(newLine);
+    }
+    return parsed_data;
+  }
+}
+
+module.exports = AdventOfCode;
