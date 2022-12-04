5b512fc26d880769d8130ec4953f62bb15e463d5
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-12-04 09:02:28 +0100

    2022 day 04 part 1

---
+++ 2022/04/data.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const sampleInputs = [
+  {
+    input: `2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8`
+      .split("\n")
+      .map((elem) => elem.trim()),
+    part_one: 2,
+    part_two: undefined,
+  },
+  {
+    input: ``.split("\n").map((elem) => elem.trim()),
+    part_one: undefined,
+    part_two: undefined,
+  },
+];
+
+module.exports = {
+  input,
+  sampleInputs,
+};
---
+++ 2022/04/index.js
@@ -0,0 +1,44 @@
+class AdventOfCode {
+  constructor(raw_data) {
+    this.infos = this.parseInfos(raw_data);
+  }
+
+  part_one() {
+    let count = 0;
+    for (let line of this.infos) {
+      if (
+        (line.first.start >= line.second.start &&
+          line.first.end <= line.second.end) ||
+        (line.first.start <= line.second.start &&
+          line.first.end >= line.second.end)
+      ) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  part_two() {
+    return true;
+  }
+
+  parseInfos(raw_data) {
+    const parsed_data = [];
+    for (let line of raw_data) {
+      line = line.split(",").map((elem) => elem.split("-"));
+      parsed_data.push({
+        first: {
+          start: Number(line[0][0]),
+          end: Number(line[0][1]),
+        },
+        second: {
+          start: Number(line[1][0]),
+          end: Number(line[1][1]),
+        },
+      });
+    }
+    return parsed_data;
+  }
+}
+
+module.exports = AdventOfCode;
