03e15e3766a3505f7aa10206416a3d5014516e49
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-12-05 22:42:08 +0100

    2022 day 05 part 1

---
+++ 2022/05/data.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .split("\n");
+
+const sampleInputs = [
+  {
+    input: `    [D]    
+[N] [C]    
+[Z] [M] [P]
+     1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`.split("\n"),
+    part_one: 'CMZ',
+    part_two: undefined,
+  },
+  {
+    input: ``.split("\n").map((elem) => elem.trim()),
+    part_one: undefined,
+    part_two: undefined,
+  },
+];
+
+module.exports = {
+  input,
+  sampleInputs,
+};
---
+++ 2022/05/index.js
@@ -0,0 +1,67 @@
+class AdventOfCode {
+  constructor(raw_data) {
+    this.infos = this.parseInfos(raw_data);
+    this.stacks = this.infos[0];
+    this.moves = this.infos[1];
+  }
+
+  part_one() {
+    for (let move of this.moves) {
+      this.moveStack(move);
+    }
+    return this.getTopLetters();
+  }
+
+  getTopLetters() {
+    const result = [];
+    Object.values(this.stacks).forEach((arr) =>
+      result.push(arr[arr.length - 1])
+    );
+    return result.join("");
+  }
+
+  moveStack(move) {
+    for (let i = 0; i < move.quantity; i++) {
+      const item_to_move = this.stacks[move.from].pop();
+      this.stacks[move.to].push(item_to_move);
+    }
+  }
+
+  part_two() {
+    return true;
+  }
+
+  parseInfos(raw_data) {
+    const stacks = {
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: [],
+    };
+    const moves = [];
+    const separator = raw_data.indexOf("");
+    const raw_stacks = raw_data.slice(0, separator - 1);
+    const raw_moves = raw_data.slice(separator + 1);
+    for (let line of raw_stacks) {
+      for (let i = 0; i < line.length; i += 4) {
+        const idx = i + 1;
+        if (line[idx] !== " ") {
+          const idx_to_add = Math.ceil(idx / 4);
+          stacks[idx_to_add].unshift(line[idx]);
+        }
+      }
+    }
+    for (let line of raw_moves) {
+      line = line.split(" ");
+      moves.push({ quantity: Number(line[1]), from: line[3], to: line[5] });
+    }
+    return [stacks, moves];
+  }
+}
+
+module.exports = AdventOfCode;
