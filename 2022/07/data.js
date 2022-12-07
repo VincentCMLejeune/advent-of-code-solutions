7c6df2af9d59a216ccdf96d492c4f03796d99ccc
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-12-07 12:11:14 +0100

    2022 day 07 part 1

---
+++ 2022/07/data.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const sampleInputs = [
+  {
+    input: `$ ls
+    dir a
+    14848514 b.txt
+    8504156 c.dat
+    dir d
+    $ cd a
+    $ ls
+    dir e
+    29116 f
+    2557 g
+    62596 h.lst
+    $ cd e
+    $ ls
+    584 i
+    $ cd ..
+    $ cd ..
+    $ cd d
+    $ ls
+    4060174 j
+    8033020 d.log
+    5626152 d.ext
+    7214296 k`.split("\n").map((elem) => elem.trim()),
+    part_one: 95437,
+    part_two: undefined,
+  },
+  {
+    input: ``.split("\n").map((elem) => elem.trim()),
+    part_one: undefined,
+    part_two: undefined,
+  },
+];
+
+module.exports = {
+  input,
+  sampleInputs,
+};
---
+++ 2022/07/index.js
@@ -0,0 +1,107 @@
+class AdventOfCode {
+  constructor(raw_data) {
+    this.instructions = this.parseInfos(raw_data);
+    this.fs = { "/": { files: [] } };
+    this.current_path = ["/"];
+    this.directories = [{ name: "/", path: ["/"] }];
+  }
+
+  part_one() {
+    let res = 0;
+    while (this.instructions.length !== 0) {
+      let instruction = this.instructions.shift();
+      if (instruction === "$ ls") {
+        this.listFiles();
+      } else {
+        this.changeDirectory(instruction);
+      }
+    }
+    this.printTree();
+    for (let dir of this.directories) {
+      let size = this.getSize(dir.path);
+      if (size <= 100000) res += size;
+    }
+    return res;
+  }
+
+  getSize(path) {
+    let tree = this.fs;
+    for (let elem of path) {
+      tree = tree[elem];
+    }
+    let size = 0;
+    let subFolders = [];
+    for (let elem of Object.keys(tree)) {
+      if (elem === "files") {
+        for (let file of tree[elem]) {
+          size += file.size;
+        }
+      } else {
+        size += this.getSize([...path, elem]);
+      }
+    }
+    return size;
+  }
+
+  printTree(path = ["/"]) {
+    let tree = this.fs;
+    let name = path[path.length - 1];
+    for (let elem of path) {
+      tree = tree[elem];
+    }
+    for (let elem of Object.keys(tree).filter((item) => item !== "files")) {
+      this.directories.push({ name: elem, path: [...path, elem] });
+      this.printTree([...path, elem]);
+    }
+  }
+
+  changeDirectory(instruction) {
+    instruction = instruction.split(" ");
+    if (instruction[2] === "..") {
+      this.current_path.pop();
+    } else {
+      this.current_path.push(instruction[2]);
+    }
+  }
+
+  listFiles() {
+    while (this.instructions.length !== 0 && this.instructions[0][0] !== "$") {
+      let newElement = this.instructions.shift();
+      newElement = newElement.split(" ");
+      if (newElement[0] === "dir") {
+        this.addDirToPath(newElement[1]);
+      } else {
+        this.addFileToPath({
+          name: newElement[1],
+          size: Number(newElement[0]),
+        });
+      }
+    }
+  }
+
+  addFileToPath(file) {
+    let path = this.fs;
+    for (let elem of this.current_path) {
+      path = path[elem];
+    }
+    path.files.push(file);
+  }
+
+  addDirToPath(dirName) {
+    let path = this.fs;
+    for (let elem of this.current_path) {
+      path = path[elem];
+    }
+    path[dirName] = { files: [] };
+  }
+
+  part_two() {
+    return true;
+  }
+
+  parseInfos(raw_data) {
+    return raw_data;
+  }
+}
+
+module.exports = AdventOfCode;
