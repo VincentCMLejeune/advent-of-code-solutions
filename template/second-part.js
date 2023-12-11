e8f9f4a906cbee2c21955ad2b469d8215af11f4f
Vincent Lejeune <vincentcmlejeune@gmail.com>
2023-12-11 09:16:19 +0100

    Updated template (now displays time to process answers)

--- template/second-part.js
+++ template/second-part.js
@@ -5,14 +5,18 @@ const AdventOfCode = require("./index");
 let testNum = 1;
 for (let i = 0; i < sampleInputs.length; i++) {
   if (sampleInputs[i].part_two !== undefined) {
+    const before = Date.now();
     let sample_instance = new AdventOfCode(sampleInputs[i].input);
     let sample_part_two = sample_instance.part_two();
+    const after = Date.now();
     assert.strictEqual(sample_part_two, sampleInputs[i].part_two);
-    console.log(`Test ${testNum} successful`);
+    console.log(`Test ${testNum} successful in ${after - before}ms`);
     testNum++;
   }
 }
 
+const before = Date.now();
 let instance = new AdventOfCode(input);
 let res = instance.part_two();
-console.log(`Part two answer: ${res}`);
+const after = Date.now();
+console.log(`Part two answer: ${res} (${after - before}ms)`);
